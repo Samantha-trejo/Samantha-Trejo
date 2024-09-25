ed13c93e7b0f472a1d8292bc4f1a566d1d0c1ae1
Samantha-Trejo <samantha.trejo@ucas-edu.net>
2024-09-25 09:20:02 -0600

    Celsius and Fahrenheit

---
+++ Celsius and Fahrenheit/main.py
@@ -0,0 +1,12 @@
+#Samantha Trejo, Celsius and Fahrenheit
+
+output = 0
+
+
+Celsius = int(input("enter temperature in Celsius: "))
+
+output = Celsius * 9/5 + 32
+
+
+
+print("When it is", Celsius, "degrees Celsius it is", output, "degrees fahrenheit")
