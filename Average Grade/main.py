53bf423234309d288be0c131b1630c37e2379688
Samantha-Trejo <samantha.trejo@ucas-edu.net>
2024-09-26 09:26:18 -0600

    Grades

---
+++ Average Grade/main.py
@@ -0,0 +1,11 @@
+#Samantha Trejo, Average Grade
+first=91.45
+second=99.29
+third=80.10
+fourth=100.00
+sixth=65.03
+seventh=49.05
+eighth= 67.43
+classes= 7
+
+print((first+second+third+fourth+sixth+seventh+eighth)/classes)
