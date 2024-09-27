8d7be119dc9a31283d51c2e40f128962e2c2aabd
Samantha-Trejo <samantha.trejo@ucas-edu.net>
2024-09-27 09:48:14 -0600

    Simple Calculator

---
+++ Simple_Calculator/main.py
@@ -0,0 +1,21 @@
+#Samantha Trejo, Simple Calculator
+
+output = 0
+
+x = int(input("type in x"))
+y = int(input("type in y"))
+
+#addition
+print(x + y)
+#subraction
+print(x - y)
+#multiplication
+print(x * y)
+#division 
+print(x / y)
+#modulus
+print(x % y)
+#exponentiation
+print(x ** y)
+#floor division
+print(x // y)
