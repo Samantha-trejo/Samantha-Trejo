c4981756846fe0d273630144b8cc2b1b777a8561
Samantha-Trejo <samantha.trejo@ucas-edu.net>
2024-10-02 09:25:13 -0600

    Graded Quiz

---
+++ Graded Quiz/main.py
@@ -0,0 +1,22 @@
+#Samantha Trejo, Graded Quiz
+
+score = 0
+
+Question = int(input("What is 2*2"))
+if Question == 4:
+    score +=1
+
+Question = int(input("What is 20-2?"))
+if Question == 18:
+    score +=1
+
+Question = int(input("What is 35+24"))
+if Question == 59:
+    score +=1
+
+Question = int(input("What is 10/2"))
+if Question == 5:
+    score +=1
+
+
+print("your score out of 4 is", score )
