fe6b224cae52ec30cc30ec06b6b40776279114e6
Samantha-Trejo <samantha.trejo@ucas-edu.net>
2024-10-04 09:52:09 -0600

    Madlib

---
+++ Madlib/main.py
@@ -0,0 +1,19 @@
+#Samantha Trejo, Madlib
+
+place = (input("Type in a place"))
+Animal = (input("Type in a animal"))
+Name = (input("Type in any name"))
+Store = (input("type in a store"))
+Money = (input("type in amount of money"))
+
+
+a = "I went to walk by the"
+b = "While walking there I saw a"
+c = "I went to walk by the", Animal, "and it bit me"
+d = "I ran away as I saw"
+e = "They told me it was a nice", Animal, "I just need to talk to it"
+f = "I told", Name, "no and went to the", Store
+h = "In the store i noticed i only had"
+i = "so instead I left"
+
+print(a + place)
---
+++ Palindrome/main.py
@@ -0,0 +1,11 @@
+#Samantha Trejo, Palindrome
+
+words = 1
+
+word = int(input("Type in a palindrome"))
+
+if word : words
+else:
+    print("This word isnt a palinndrome")
+
+
