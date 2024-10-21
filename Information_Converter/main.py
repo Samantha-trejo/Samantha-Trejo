80a2159ab8c8146b0524e7a865348526e61c0a85
Samantha-Trejo <samantha.trejo@ucas-edu.net>
2024-10-21 09:47:39 -0600

    Information Converter

---
+++ Information_Converter/main.py
@@ -0,0 +1,19 @@
+#Samantha trejo, Information Converter
+
+#string
+name = input("What is your name?")
+
+#intergers
+age = input("How old are you?")
+favorite_number = input("What is your favorite number?")
+
+#float
+height = input("How tall are you in meters?")
+
+
+A = str(name)
+B = int(age)
+C = int(favorite_number)
+D = float(height)
+
+print(A, B, C, D)
