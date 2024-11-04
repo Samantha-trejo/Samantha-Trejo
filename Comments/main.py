643b962b655d5f89fd5089e58ac4cb87c2ca5894
Samantha-trejo <samantha.trejo@ucas-edu.net>
2024-11-04 09:36:52 -0700

    Comments

---
+++ Comments/main.py
@@ -0,0 +1,9 @@
+#Samantha Trejo, Comments - Creating a code the prints the users name
+
+
+#asking for the users name
+Name = str(input("What is your name?"))
+
+
+#printing "hello" and the users name
+print("hello", Name)
