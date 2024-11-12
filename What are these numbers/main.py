51bc8550facfe7a35c6d6a97acfaad3284c102df
Samantha-trejo <samantha.trejo@ucas-edu.net>
2024-11-12 09:16:55 -0700

    What are these numbers

--- What are these numbers/main.py
+++ What are these numbers/main.py
@@ -1,2 +1,22 @@
 #Samantha Trejo, What are these numbers?
 
+#Get the number from the user
+number = float(input("Enter a number: "))
+
+#Format as an integer with commas
+formatted_integer = "{:,}".format(int(number))
+
+#Format as a float with 4 decimal places
+formatted_float = "{:.4f}".format(number)
+
+#Format as a percentage
+formatted_percentage = "{:.2%}".format(number / 100)
+
+#Format as a dollar amount
+formatted_dollar = "${:,.2f}".format(number)
+
+#Print the results
+print("Formatted as an integer with commas:", formatted_integer)
+print("Formatted as a float with 4 decimal places:", formatted_float)
+print("Formatted as a percentage:", formatted_percentage)
+print("Formatted as a dollar amount:", formatted_dollar)
